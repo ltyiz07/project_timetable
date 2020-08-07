5e7be68af73396ebee7bfff86b2f60f717ad05bf
leety <ltyiz07@gmail.com>
2020-08-07 17:41:41 +0900

    08/07 too many output problem

--- set_trial.py
+++ set_trial.py
@@ -6,6 +6,7 @@
 # 5. 물리_1(수4~6), 물리_2(금5~7)
 # """
 
+# 결과 변수 너무 많음 제한이나 추가 옵션 추가 필!
 engineering = [{"mon_1", "mon_2", "mon_3"}, {"mon_4", "mon_5", "mon_6"}, {"wed_1", "wed_2", "wed_3"}]
 mathematic = [{"mon_5", "mon_6", "mon_7"}, {"thu_2", "thu_3", "thu_4"}, {"fri_1", "fri_2", "fri_3"}]
 experiment = [{"mon_1", "mon_2", "mon_3", "wed_1", "wed_2", "wed_3"},
@@ -13,6 +14,22 @@ experiment = [{"mon_1", "mon_2", "mon_3", "wed_1", "wed_2", "wed_3"},
 coding = [{"tue_2", "tue_3"}, {"wed_2", "wed_3"}, {"thu_2", "thu_3"}]
 physics = [{"wed_4", "wed_5", "wed_6"}, {"fri_5", "fri_6", "fri_7"}]
 
-for i in mathematic:
-    if engineering[0].isdisjoint(i):
-        print(i)
+classlist = []
+
+for j in engineering:
+    for i in mathematic:
+        if j.isdisjoint(i):
+            eng_math = j.union(i)
+        for k in experiment:
+            if eng_math.isdisjoint(k):
+                eng_math_exp = eng_math.union(k)
+                for m in coding:
+                    if eng_math_exp.isdisjoint(m):
+                        eng_math_exp_cod = eng_math_exp.union(m)
+                        for n in physics:
+                            if eng_math_exp_cod.isdisjoint(n):
+                                eng_math_exp_cod_phy = eng_math_exp_cod.union(n)
+                                classlist.append(eng_math_exp_cod_phy)
+
+
+print(len(classlist), classlist)
