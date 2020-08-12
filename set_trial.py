c9e51e971e17a0fbb5b03fd71cbfbb53434c5db2
Young <ltyiz07@gmail.com>
2020-08-12 21:29:06 +0900

    08/12 trial with class but not easy...

--- set_trial.py
+++ set_trial.py
@@ -13,6 +13,7 @@ experiment = [{"mon_1", "mon_2", "mon_3", "wed_1", "wed_2", "wed_3"},
               {"tue_1", "tue_2", "tue_3", "thu_1", "thu_2", "thu_3"}]
 coding = [{"tue_2", "tue_3"}, {"wed_2", "wed_3"}, {"thu_2", "thu_3"}]
 physics = [{"wed_4", "wed_5", "wed_6"}, {"fri_5", "fri_6", "fri_7"}]
+all_classses = [engineering, mathematic, experiment, coding, physics]
 
 classlist = []
 sequances = []
---
+++ test_1.py
@@ -0,0 +1,6 @@
+engineering = [{"mon_1", "mon_2", "mon_3"}, {"mon_4", "mon_5", "mon_6"}, {"wed_1", "wed_2", "wed_3"}]
+mathematic = [{"mon_5", "mon_6", "mon_7"}, {"thu_2", "thu_3", "thu_4"}, {"fri_1", "fri_2", "fri_3"}]
+experiment = [{"mon_1", "mon_2", "mon_3", "wed_1", "wed_2", "wed_3"},
+              {"tue_1", "tue_2", "tue_3", "thu_1", "thu_2", "thu_3"}]
+coding = [{"tue_2", "tue_3"}, {"wed_2", "wed_3"}, {"thu_2", "thu_3"}]
+physics = [{"wed_4", "wed_5", "wed_6"}, {"fri_5", "fri_6", "fri_7"}]
